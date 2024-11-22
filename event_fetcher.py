aca1f504088bf0ee7163fd8e9385fa8a973216d6
Christoph <christoph.seufert@gmail.com>
2024-11-23 00:02:15 +0100

    working

--- event_fetcher.py
+++ event_fetcher.py
@@ -9,8 +9,8 @@ from datetime import datetime, timedelta
 URL = 'https://ra.co/graphql'
 HEADERS = {
     'Content-Type': 'application/json',
-    'Referer': 'https://ra.co/events/uk/london',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:106.0) Gecko/20100101 Firefox/106.0'
+    'Referer': 'https://ra.co/events/de/cologne',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36'
 }
 QUERY_TEMPLATE_PATH = "graphql_query_template.json"
 DELAY = 1  # Adjust this value as needed
